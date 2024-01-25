d2d828a10f5a6be991d184bc32202ab208a7a36b
r0usta <levder@spsejecna.cz>
2024-01-25 18:02:50 +0100

    Add config initialization.

--- DatabaseSystemAlfa/Program.cs
+++ DatabaseSystemAlfa/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DatabaseSystemAlfa.Configuration;
+using DatabaseSystemAlfa.Configuration.Structs;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
 
-Console.WriteLine("Hello, World!");
+AppSettings appSettings;
+
+try
+{
+    IConfigurationRoot configurationRoot = AppConfigurator.Build("app-settings.json");
+    appSettings = new AppSettings(configurationRoot);
+}
+catch (Exception e)
+{
+    AnsiConsole.Markup("[red]{0}[/]", e.Message);
+}
