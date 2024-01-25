779b77a0fcc2274e0eae1294d03b259c89f147f5
r0usta <levder@spsejecna.cz>
2024-01-25 17:59:58 +0100

    Upload `DatabaseConfig` struct-record.

---
+++ DatabaseSystemAlfa/Configuration/Structs/DatabaseConfig.cs
@@ -0,0 +1,71 @@
+namespace DatabaseSystemAlfa.Configuration.Structs;
+
+public record struct DatabaseConfig
+{
+    private readonly string _host;
+    private readonly string _user;
+    private readonly string _name;
+    private readonly int _port;
+
+    public DatabaseConfig(string host, string user, string password, string name, int port)
+    {
+        Host = host;
+        User = user;
+        Password = password;
+        Name = name;
+        Port = port;
+    }
+    
+    public string Host
+    {
+        get => _host;
+        [System.Diagnostics.CodeAnalysis.MemberNotNull("_host")]
+        init
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("Server database host cannot be null or empty.", nameof(Host));
+
+            _host = value;
+        }
+    }
+    
+    public string User
+    {
+        get => _user;
+        [System.Diagnostics.CodeAnalysis.MemberNotNull("_user")]
+        init
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("Database user cannot be null or empty.", nameof(User));
+
+            _user = value;
+        }
+    }
+    
+    public string Password { get; init; }
+
+    public string Name
+    {
+        get => _name;
+        [System.Diagnostics.CodeAnalysis.MemberNotNull("_name")]
+        init
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("Database name cannot be null or empty.", nameof(Name));
+
+            _name = value;
+        }
+    }
+    
+    public int Port
+    {
+        get => _port;
+        init
+        {
+            if (value is <= 0 or > 65535)
+                throw new ArgumentOutOfRangeException(nameof(value), "Database port must be in the range 1 to 65535.");
+
+            _port = value;
+        }
+    }
+}
