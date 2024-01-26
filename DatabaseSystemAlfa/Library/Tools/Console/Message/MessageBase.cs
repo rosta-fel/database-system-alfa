88f10c6843e876269886dcd8db13d24495c83b9b
r0usta <levder@spsejecna.cz>
2024-01-26 21:27:30 +0100

    Create abstarct class with event handler for message displaying

---
+++ DatabaseSystemAlfa/Library/Tools/Console/Message/MessageBase.cs
@@ -0,0 +1,18 @@
+namespace DatabaseSystemAlfa.Library.Tools.Console.Message;
+
+public abstract class MessageBase(string message) : IDisplayHandler
+{
+    public static event EventHandler<string>? DisplayRequestedEvent;
+
+    public void Display()
+    {
+        OnDisplayRequested(message);
+    }
+
+    private void OnDisplayRequested(string messageInput)
+    {
+        DisplayRequestedEvent?.Invoke(this, messageInput);
+    }
+    
+    public override string ToString() => message;
+}
