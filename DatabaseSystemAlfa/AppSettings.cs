e26091fc182bd76f816317bb6014e97fbefb41f2
r0usta <levder@spsejecna.cz>
2024-02-01 13:42:09 +0100

    Add connection string get method

--- DatabaseSystemAlfa/AppSettings.cs
+++ DatabaseSystemAlfa/AppSettings.cs
@@ -1,30 +1,34 @@
-using DatabaseSystemAlfa.Library.Configuration.Exceptions;
-using DatabaseSystemAlfa.Library.Configuration.Structs;
+using DatabaseSystemAlfa.Libraries.Configuration.Exceptions;
+using DatabaseSystemAlfa.Libraries.Configuration.Settings;
 using Microsoft.Extensions.Configuration;
 
 namespace DatabaseSystemAlfa;
 
 public class AppSettings
 {
-    public DatabaseConfig DatabaseConfig { get; }
+    public DbConnectionSettings DbConnectionSettings { get; }
 
     public AppSettings()
     {
-        DatabaseConfig = new DatabaseConfig
+        DbConnectionSettings = new DbConnectionSettings
         {
-            Host = "localhost",
-            User = "root",
+            Server = "127.0.0.1",
+            Uid = "root",
+            Password = "root",
+            Database = "database_system_alfa_db",
             Port = 3306
         };
     }
 
     public AppSettings(IConfiguration conf)
     {
-        IConfigurationSection dbConfSection = conf.GetSection(nameof(DatabaseConfig));
+        IConfigurationSection dbConfSection = conf.GetSection(nameof(DbConnectionSettings));
 
         if (!dbConfSection.GetChildren().Any())
-            throw new InvalidConfigException("Invalid or missing section in the configuration file: ", nameof(DatabaseConfig));
+            throw new InvalidConfigException("Invalid or missing section in the configuration file: ", nameof(DbConnectionSettings));
 
-        DatabaseConfig = dbConfSection.Get<DatabaseConfig>();
+        DbConnectionSettings = dbConfSection.Get<DbConnectionSettings>();
     }
+
+    public string GetConnectionString() => DbConnectionSettings.ToString();
 }
