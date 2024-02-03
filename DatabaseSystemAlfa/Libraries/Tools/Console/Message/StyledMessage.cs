1f25d28f9ba817dc7ef1d3197337430f7aa8b66d
r0usta <levder@spsejecna.cz>
2024-02-03 23:23:57 +0100

    Add new two styled messages methods

--- DatabaseSystemAlfa/Libraries/Tools/Console/Message/StyledMessage.cs
+++ DatabaseSystemAlfa/Libraries/Tools/Console/Message/StyledMessage.cs
@@ -1,5 +1,6 @@
 namespace DatabaseSystemAlfa.Libraries.Tools.Console.Message;
 
+// TODO: Rename to PatternMessage, TemplateMessage or LayoutMessage
 public class StyledMessage(string message) : MessageBase(message)
 {
     public static StyledMessage Info(string message) => new($"[cyan]INFO[/]: [gray]{message}[/]");
@@ -9,4 +10,8 @@ public class StyledMessage(string message) : MessageBase(message)
     public static StyledMessage Warning(string message) => new($"[yellow]WARN[/]: [gray]{message}[/]");
 
     public static StyledMessage Error(string message) => new($"[red]ERROR[/]: [gray]{message}[/]");
+    
+    public static StyledMessage Tip(string message) => new($"[blue]TIP[/]: [gray]{message}[/]");
+
+    public static StyledMessage Title(string message) => new($"[bold]{message}[/]");
 }
