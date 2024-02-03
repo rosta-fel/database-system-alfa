b16fc18f34f7c72e15fa2799d1d67d1efb334e5e
r0usta <levder@spsejecna.cz>
2024-02-03 23:27:40 +0100

    Add not implemented setup config operation class

---
+++ DatabaseSystemAlfa/Services/Operations/Menu/SetupConfigurationOperation.cs
@@ -0,0 +1,9 @@
+namespace DatabaseSystemAlfa.Services.Operations.Menu;
+
+public class SetupConfigurationOperation : IOperation
+{
+    public OperationResult Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
