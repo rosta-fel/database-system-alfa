107f4c19e3e307ba034d6bd26e7a3b3462d7d597
r0usta <levder@spsejecna.cz>
2024-02-04 17:59:59 +0100

    Add two more custom prompts to template

--- DatabaseSystemAlfa/Libraries/Tools/Console/Prompt/PromptTemplate.cs
+++ DatabaseSystemAlfa/Libraries/Tools/Console/Prompt/PromptTemplate.cs
@@ -1,10 +1,31 @@
 using DatabaseSystemAlfa.Libraries.Tools.Console.Message;
+using DatabaseSystemAlfa.Libraries.Tools.Console.Message.Extensions;
 using Spectre.Console;
 
 namespace DatabaseSystemAlfa.Libraries.Tools.Console.Prompt;
 
-public abstract class PromptTemplate(SelectionPrompt<string> selectionPrompt)
+public abstract class PromptTemplate<T> : PromptBase<T> where T : notnull
 {
-    public static SelectionPrompt<string> Classic(string title, IEnumerable<string> choices) =>
-        new SelectionPrompt<string>().Title(MessageTemplate.Bold(title).ToString()).AddChoices(choices);
+    public static T Selection(string title, IEnumerable<T> choices)
+    {
+        return RequestOnPrompt(new SelectionPrompt<T>().Title(MessageTemplate.Bold(title).ToString())
+            .AddChoices(choices));
+    }
+
+    public static T OptionalAndSecret(string prompt, string arg)
+    {
+        MessageTemplate msgT = MessageTemplate.HighlightArg(prompt, arg).MarkAsOptional();
+        
+        TextPrompt<T> textPrompt = new TextPrompt<T>(msgT.ToString())
+            .AllowEmpty()
+            .PromptStyle("red")
+            .Secret();
+            
+        return RequestOnPrompt(textPrompt);
+    }
+
+    public static T HighlightAsk(string promptAsk, string arg)
+    {
+        return RequestOnAsk(MessageTemplate.HighlightArg(promptAsk, arg).ToString());
+    }
 }
