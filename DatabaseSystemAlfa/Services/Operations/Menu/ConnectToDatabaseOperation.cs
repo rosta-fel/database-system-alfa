f2859d717eb536a7658439f5c49c4bbf2d7c1a4a
r0usta <levder@spsejecna.cz>
2024-02-04 18:02:03 +0100

    Replace old operation result state with new one

--- DatabaseSystemAlfa/Services/Operations/Menu/ConnectToDatabaseOperation.cs
+++ DatabaseSystemAlfa/Services/Operations/Menu/ConnectToDatabaseOperation.cs
@@ -1,3 +1,4 @@
+using DatabaseSystemAlfa.Libraries.Tools.Console.Message;
 using MySql.Data.MySqlClient;
 using Spectre.Console;
 
@@ -9,40 +10,39 @@ public class ConnectToDatabaseOperation(AppSettings appSettings, Status connStat
     public OperationResult Execute()
     {
         DatabaseSingleton.Instance.Initialize(appSettings.GetConnectionString());
-    
+
         MySqlException lastMySqlException = null!;
         OperationResult? result = null;
-    
+
         connStatus.Start("Connecting to database...", ctx =>
         {
             for (int retry = 1; retry <= maxConnRetries; retry++)
-            {
                 try
                 {
                     DatabaseSingleton.Instance.Connection?.Open();
-                    result = new OperationResult(true, "Successfully created connection with the database");
+                    result = new OperationResult(
+                        MessageTemplate.Success("Successfully created connection with the database"));
                     return;
                 }
                 catch (MySqlException mySqlException)
                 {
                     lastMySqlException = mySqlException;
-                
+
                     if (retry == 1) ctx.Spinner(Spinner.Known.GrowVertical);
                     ctx.Status($"(Error) {retry}/{maxConnRetries} Attempting to connect...");
 
                     Thread.Sleep(1000 * retry);
                 }
-            }
         });
 
         if (result is not null) return result;
-        
+
         DatabaseSingleton.Instance.CloseConnection();
 
         return new OperationResult(
-            false,
-            lastMySqlException.Message,
-            "Ensure the database server is running, and verify that the connection configuration is correct"
-        );
+            MessageTemplate.Error(lastMySqlException.Message),
+            MessageTemplate.Tip(
+                "Ensure the database server is running, and verify that the connection configuration is correct"
+            ));
     }
 }
