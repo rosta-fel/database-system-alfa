748d3742c83acef6124c0704bdb4f8a3bd80c903
r0usta <levder@spsejecna.cz>
2024-02-04 18:02:20 +0100

    Add new operation to handle configuration saving

---
+++ DatabaseSystemAlfa/Services/Operations/Menu/SaveConfigurationOperation.cs
@@ -0,0 +1,20 @@
+using DatabaseSystemAlfa.Libraries.Configuration;
+using DatabaseSystemAlfa.Libraries.Tools.Console.Message;
+
+namespace DatabaseSystemAlfa.Services.Operations.Menu;
+
+public class SaveConfigurationOperation(AppSettings appSettings) : IOperation
+{
+    public OperationResult Execute()
+    {
+        try
+        {
+            Configurator.SerializeToJson(appSettings, true);
+            return new OperationResult(MessageTemplate.Success("The configuration was successfully saved in the root folder of the executable."));
+        }
+        catch (Exception e)
+        {
+            return new OperationResult(MessageTemplate.Error(e.Message));
+        }
+    }
+}
