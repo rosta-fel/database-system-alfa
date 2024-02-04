5576861d39a0302f5145bc57bbebb2cc0512758c
r0usta <levder@spsejecna.cz>
2024-02-04 17:52:31 +0100

    Add constructor to database connection settings struct

--- DatabaseSystemAlfa/Libraries/Configuration/Settings/DbConnectionSettings.cs
+++ DatabaseSystemAlfa/Libraries/Configuration/Settings/DbConnectionSettings.cs
@@ -3,13 +3,13 @@ using System.Text;
 
 namespace DatabaseSystemAlfa.Libraries.Configuration.Settings;
 
-public readonly struct DbConnectionSettings
+public readonly struct DbConnectionSettings(string server, string database, string uid, string password, int port)
 {
-    public string Server { get; init; }
-    public string Database { get; init; }
-    public string Uid { get; init; }
-    public string Password { get; init; }
-    public int Port { get; init; }
+    public string Server { get; init; } = server;
+    public string Database { get; init; } = database;
+    public string Uid { get; init; } = uid;
+    public string Password { get; init; } = password;
+    public int Port { get; init; } = port;
 
 
     public override string ToString()
